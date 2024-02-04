5d4efc310a03003d740bd2d684189b87fa160f54
mmizon <157143739+mmizon@users.noreply.github.com>
2024-02-04 23:21:45 +0000

    circles

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ function setup(){
 }
 
 function draw(){
-  background(200);
+  background(0, 0, 255);
   // add your drawing code here ...
-  
+  ellipse(200,100,75)
+  ellipse(170, 65, 30)
+  ellipse(230, 135, 30)
+  ellipse(230, 65, 30)
+  ellipse(170, 135, 30)
+  ellipse(200, 50, 40, 50)
+  ellipse(200, 140, 10, 20)
 }
