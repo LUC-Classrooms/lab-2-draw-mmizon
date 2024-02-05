74e6479fc941a177335e6f719c342a74c9ade2ce
mmizon <157143739+mmizon@users.noreply.github.com>
2024-02-05 21:40:04 +0000

    lab complete turtle

--- script.js
+++ script.js
@@ -6,10 +6,21 @@ function draw(){
   background(0, 0, 255);
   // add your drawing code here ...
   ellipse(200,100,75)
+  fill (0, 163, 108)
   ellipse(170, 65, 30)
   ellipse(230, 135, 30)
   ellipse(230, 65, 30)
   ellipse(170, 135, 30)
   ellipse(200, 50, 40, 50)
-  ellipse(200, 140, 10, 20)
+  triangle (195, 135, 205, 135, 200, 150)
+  arc(200, 40, 20, 20, PI, TWO_PI) //mouth
+  ellipse(190, 50, 3) //eyes
+  ellipse(210, 50, 3)
+  strokeWeight(1)
+  line(198, 45, 198, 43)
+  line(202, 45, 202, 43)
+  strokeWeight(2)
+  arc(185, 90, 15, 15, TWO_PI, PI) //scutes
+  arc(215, 100, 15, 15, TWO_PI, PI)
+  arc(195,110, 15, 15, TWO_PI, PI)
 }
